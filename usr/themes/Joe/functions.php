7906600dfdd69dfabf4ea14dd76a909fe2b06f8b
小宇~ <95265298+xiaoyu171222853@users.noreply.github.com>
2023-02-18 12:47:59 +0800

    Update functions.php

--- usr/themes/Joe/functions.php
+++ usr/themes/Joe/functions.php
@@ -223,13 +223,10 @@ function themeConfig($form)
 
   $JFooter_Right = new Typecho_Widget_Helper_Form_Element_Textarea(
     'JFooter_Right',
-    '<a href="https://www.xiaoyudi.xyz" target="_blank" rel="noopener noreferrer">首页</a>
-         <a href="https://78.al/sitemap.xml" target="_blank" rel="noopener noreferrer" style="margin-left: 15px">MAP</a>',
+    NULl,
     '<a href="https://www.xiaoyudi.xyz" target="_blank" rel="noopener noreferrer">首页</a>
          <a href="https://78.al/sitemap.xml" target="_blank" rel="noopener noreferrer" style="margin-left: 15px">MAP</a>',
     '自定义底部栏右侧内容（非必填）',
-    '介绍：用于修改全站底部右侧内容（wap端下方） <br>
-         例如：&lt;a href="/"&gt;首页&lt;/a&gt; &lt;a href="/"&gt;关于&lt;/a&gt;'
   );
   $JFooter_Right->setAttribute('class', 'joe_content joe_global');
   $form->addInput($JFooter_Right);
